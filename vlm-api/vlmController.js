615fd7261b2c0c21b9e394a3afe155e122230b2a
Vaibhav Muchandi <vaibhavmm@hotmail.com>
2020-01-24 20:40:46 +0530

    Added route comments

--- vlm-api/vlmController.js
+++ vlm-api/vlmController.js
@@ -8,61 +8,67 @@ router.use(bodyParser.json());
 var vlm = require("./FabricHelper")
 
 
-// Request LC
+// Create Car
 router.post('/createCar', function (req, res) {
 
     vlm.createCar(req, res);
 
 });
 
-// Issue LC
+// Transfer Car to Dealer
 router.post('/transferCar', function (req, res) {
 
     vlm.transferCar(req, res);
 
 });
 
-// Accept LC
+// Sell and register car to customer
 router.post('/sellnRegisterCar', function (req, res) {
 
     vlm.sellnRegisterCar(req, res);
 
 });
 
-// Get LC
+// Get car details
 router.post('/getCar', function (req, res) {
 
     vlm.getCar(req, res);
 
 });
 
-//Get LC history
+//Scrap car
 router.post('/scrapCar', function (req, res) {
 
     vlm.scrapCar(req, res);
 
 });
 
+//Get car history
 router.post('/getCarHistory', function (req, res) {
     vlm.getCarHistory(req, res);
 });
 
+//Issue challan
 router.post('/issueChallan', function (req, res) {
     vlm.issueChallan(req, res);
 });
 
+//Pay challan
 router.post('/payChallan', function (req, res) {
     vlm.payChallan(req, res);
 });
 
+//Register insurance claim
 router.post('/registerClaim', function (req, res) {
     vlm.registerClaim(req, res);
 });
 
+//Pay Car loan
 router.post('/payCarLoan', function (req, res) {
     vlm.payCarLoan(req, res);
 });
 
+//Get car by regis no
 router.post('/getCarByNumber', function (req, res) {
     vlm.getCarByNumber(req, res);
 });
